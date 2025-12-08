ac424d6ea4e6edb806e08c1c67c58c0fa78c4e4b
jorgefariasso <jorge.fariasso@usm.cl>
2025-12-08 17:35:46 -0300

    first commit

---
+++ src/main/java/com/jifs/backend/CrudUsersWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.jifs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsersWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsersWithJwtApplication.class, args);
+	}
+
+}
