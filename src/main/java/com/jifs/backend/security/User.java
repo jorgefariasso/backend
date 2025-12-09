80c755821213717695d90190d9b3b24aa077f481
jorgefariasso <jorge.fariasso@usm.cl>
2025-12-09 14:18:46 -0300

    add Getter & Setter

--- src/main/java/com/jifs/backend/security/User.java
+++ src/main/java/com/jifs/backend/security/User.java
@@ -15,11 +15,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-@AllArgsConstructor @NoArgsConstructor @Builder
+@AllArgsConstructor @NoArgsConstructor @Builder @Getter @Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
